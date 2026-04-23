495fb1a4049e3c3f1594bce893f249f805e4a7e7
Matías Castillo <matcasti@umag.cl>
2026-04-22 22:58:04 -0300

    final data and preparation scripts used on raw data

--- data-raw/prep-data.R
+++ data-raw/prep-data.R
@@ -67,22 +67,95 @@ rm(to_remove)
 # -------------------------------------------------------------------------
 
 ## Text variables but are supposed to be number
-to_modidy <- sapply(raw_data, function(x) {
+to_modify <- sapply(raw_data, function(x) {
   is.character(x) & any(grepl("[0-9]", x)) & !any(grepl("[a-zA-Z]", x))
 }) |> which()
 
-raw_data[, .SD, .SDcols = to_modidy]
+raw_data[, .SD, .SDcols = to_modify]
 
 ## Variables with wrong number format (comma instead of dot) ==> to number
-raw_data[, (to_modidy) := lapply(.SD, function(x) {
+raw_data[, (to_modify) := lapply(.SD, function(x) {
   x <- gsub(pattern = "\\,", replacement = "\\.", x = trimws(x))
   x[x == ""] <- NA_character_
   as.numeric(x)
-}), .SDcols = to_modidy]
+}), .SDcols = to_modify]
+
+rm(to_modify)
 
 
 # -------------------------------------------------------------------------
 
+raw_data[
+  j = sociodemografico_long_covid_symptoms_2___sum :=
+    sociodemografico_long_covid_symptoms_2___fatigue +
+    sociodemografico_long_covid_symptoms_2___dyspnea +
+    sociodemografico_long_covid_symptoms_2___memory +
+    sociodemografico_long_covid_symptoms_2___concentration +
+    sociodemografico_long_covid_symptoms_2___sleep +
+    sociodemografico_long_covid_symptoms_2___cough +
+    sociodemografico_long_covid_symptoms_2___thoracic_pain +
+    sociodemografico_long_covid_symptoms_2___speech +
+    sociodemografico_long_covid_symptoms_2___muscle_pain +
+    sociodemografico_long_covid_symptoms_2___smell_loss +
+    sociodemografico_long_covid_symptoms_2___taste_loss +
+    sociodemografico_long_covid_symptoms_2___depression +
+    sociodemografico_long_covid_symptoms_2___anxiety +
+    sociodemografico_long_covid_symptoms_2___fever +
+    sociodemografico_long_covid_symptoms_2___other
+]
+
+raw_data[
+  j = sociodemografico_vaccine_sum :=
+    as.numeric(!is.na(sociodemografico_vaccine_1_date)) +
+    as.numeric(!is.na(sociodemografico_vaccine_2_date)) +
+    as.numeric(!is.na(sociodemografico_bivalent_vaccine_date)) +
+    as.numeric(!is.na(sociodemografico_booster_1_date)) +
+    as.numeric(!is.na(sociodemografico_booster_2_date))
+]
+
+# -------------------------------------------------------------------------
+
+## Column variables to a separate file
 names(raw_data) |>
   cat(sep = "\n",
       file = "data-raw/col_names.txt")
+
+
+# -------------------------------------------------------------------------
+
+raw_data[, sociodemografico_sex := factor(
+  x = sociodemografico_sex,
+  levels = c("male", "feminine"),
+  labels = c("Male", "Female")
+)]
+raw_data[, sociodemografico_education := factor(
+  x = sociodemografico_education,
+  levels = c("basico_incompleto", "basico_completo", "medio_incompleto", "medio_completo",
+             "tecnico_profesional_incompleto", "tecnico_profesional_completo",
+             "universitario_incompleto", "universitario_completo"),
+  ordered = TRUE
+)]
+
+covariates <-
+  raw_data[, sociodemografico_cronic_pathologies___diabetes:sociodemografico_cronic_pathologies___traumatologic_pathology] |>
+  names()
+
+raw_data[, (covariates) := lapply(.SD, factor, levels = 0:1, labels = c("No", "Yes")), .SDcols = covariates]
+
+# -------------------------------------------------------------------------
+
+## Extra underscores to single underscore
+names(raw_data) <-
+  names(raw_data) |>
+  gsub(pattern = "[_]+", replacement = "_")
+
+# -------------------------------------------------------------------------
+
+## Final data object
+pcov <- copy(raw_data); rm(raw_data)
+
+## Save data to a RData format
+save(pcov, file = "data/pcov.RData")
+
+## Save data to a csv format
+fwrite(pcov, file = "data/pcov.csv")
